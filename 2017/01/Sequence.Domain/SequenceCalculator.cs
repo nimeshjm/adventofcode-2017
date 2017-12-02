caca98ca435ae3b9789bc7fa5563545487b28680
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-02 00:07:44 +0000

    SAVEPOINT

---
+++ 2017/01/Sequence.Domain/SequenceCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sequence.Domain
+{
+    public class SequenceCalculator
+    {
+        public int Calculate(string sequence)
+        {
+            var ints = new List<int>();
+            
+            var enumerable = sequence.ToCharArray().Select(char.GetNumericValue).Select(Convert.ToInt32).ToList();
+            enumerable.Add(enumerable.First());
+
+            enumerable.Aggregate(
+                (current, next) =>
+                    {
+                        if (current == next) ints.Add(current);
+                        return next;
+                    });
+
+            return ints.Sum();
+        }
+
+        public int CalculateHalfway(string sequence)
+        {
+            var ints = new List<int>();
+
+            var enumerable = sequence.ToCharArray().Select(char.GetNumericValue).Select(Convert.ToInt32).ToList();
+
+            var delta = sequence.Length / 2;
+            for (var i = 0; i < enumerable.Count; i++)
+            {
+                if (enumerable[i] == enumerable[(delta + i) % enumerable.Count])
+                {
+                    ints.Add(enumerable[i]);
+                }
+            }
+
+            return ints.Sum();
+        }
+    }
+}
