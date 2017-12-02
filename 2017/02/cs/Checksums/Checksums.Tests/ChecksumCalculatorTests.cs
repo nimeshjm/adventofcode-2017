1699fc9d0153f595561ccfb36bdcbcafef314a55
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-02 15:12:44 +0000

    SAVEPOINT

---
+++ 2017/02/cs/Checksums/Checksums.Tests/ChecksumCalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Checksums.Domain;
+using NUnit.Framework;
+
+namespace Checksums.Tests
+{
+    [TestFixture]
+    public class ChecksumCalculatorTests
+    {
+        [Test]
+        public void TestPart1Problem()
+        {
+            var sut = new ChecksumCalculator();
+
+            var actual = sut.CalculatePart1(TestFixture("Checksums.Tests.TestData1Problem.txt"));
+
+            Assert.That(actual, Is.EqualTo(18));
+        }
+
+        [Test]
+        public void TestPart1Solution()
+        {
+            var sut = new ChecksumCalculator();
+
+            var actual = sut.CalculatePart1(TestFixture("Checksums.Tests.TestData1Solution.txt"));
+
+            Assert.That(actual, Is.EqualTo(45351));
+        }
+
+        private static List<List<int>> TestFixture(string inputFileName)
+        {
+            var lines = ReadLines(() => Assembly.GetExecutingAssembly().GetManifestResourceStream(inputFileName), Encoding.UTF8);
+
+            return lines.Select(line => line.Split(' ').Select(int.Parse).ToList()).ToList();
+        }
+
+        private static IEnumerable<string> ReadLines(Func<Stream> streamProvider, Encoding encoding)
+        {
+            using (var stream = streamProvider())
+            using (var reader = new StreamReader(stream, encoding))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
---
+++ 2017/02/cs/Checksums/Checksums/ChecksumCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Checksums.Domain
+{
+    public class ChecksumCalculator
+    {
+        public int CalculatePart1(List<List<int>> matrix)
+        {
+            return matrix.Select(innerRow => innerRow.Max() - innerRow.Min()).Sum();
+        }
+    }
+}
