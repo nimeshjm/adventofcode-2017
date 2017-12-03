07d3610acc4607c396df8557a0345b59af8b3a2c
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-03 22:33:18 +0000

    SAVEPOINT

---
+++ 2017/03/cs/Manhattan/Manhattan.Domain/DistanceCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Manhattan.Domain
+{
+    public class DistanceCalculator
+    {
+        public int CalculateShortest(int index)
+        {
+            var sqrt = Math.Sqrt(index);
+
+            if (sqrt % 1 == 0)
+            {
+                return (int)sqrt-1;
+            }
+
+            var wholeSqrt = (int)Math.Round(sqrt);
+            return Math.Abs(index - wholeSqrt*wholeSqrt);
+        }
+    }
+}
---
+++ 2017/03/cs/Manhattan/Manhattan.Tests/DistanceCalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Manhattan.Domain;
+using NUnit.Framework;
+
+namespace Manhattan.Tests
+{
+    [TestFixture]
+    public class DistanceCalculatorTest 
+    {
+        [TestCase(1, 0)]
+        [TestCase(12, 3)]
+        [TestCase(23, 2)]
+        [TestCase(1024, 31)]
+        [TestCase(312051, 430)]
+        public void Test1(int index, int expected)
+        {
+            var sut = new DistanceCalculator();
+
+            var actual = sut.CalculateShortest(index);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
