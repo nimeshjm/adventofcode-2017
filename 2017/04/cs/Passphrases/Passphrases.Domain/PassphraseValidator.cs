728e2a44ae36595ac2009baa04c47c8dc26533ae
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-04 20:52:23 +0000

    SAVEPOINT

---
+++ 2017/04/cs/Passphrases/Passphrases.Domain/PassphraseValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Passphrases.Domain
+{
+    public class PassphraseValidator
+    {
+        public bool IsValid(List<string> passphrase)
+        {
+            passphrase.Sort();
+
+            return !passphrase.Where((password, i) =>
+                {
+                    var passphraselist = passphrase.Skip(i + 1).ToArray();
+
+                    return password[0] == passphraselist.FirstOrDefault()?[0] && passphraselist.Contains(password);
+                }).Any();
+        }
+    }
+}
---
+++ 2017/04/cs/Passphrases/Passphrases.Tests/PassphraseValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using Passphrases.Domain;
+
+namespace Passphrases.Tests
+{
+    [TestFixture]
+    public class PassphraseValidatorTests
+    {
+        [TestCase("Passphrases.Tests.TestData1Problem.txt", 2)]
+        [TestCase("Passphrases.Tests.TestData1Solution.txt", 455)]
+        public void Test1(string datafile, int expected)
+        {
+            var sut = new PassphraseValidator();
+
+            var testFixture = TestFixture(datafile);
+            var actual = testFixture.Count(sut.IsValid);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+        private static List<List<string>> TestFixture(string inputFileName)
+        {
+            var lines = ReadLines(() => Assembly.GetExecutingAssembly().GetManifestResourceStream(inputFileName), Encoding.UTF8);
+
+            return lines.Select(line => line.Split(' ').ToList()).ToList();
+        }
+
+        private static IEnumerable<string> ReadLines(Func<Stream> streamProvider, Encoding encoding)
+        {
+            using (var stream = streamProvider())
+            using (var reader = new StreamReader(stream, encoding))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
