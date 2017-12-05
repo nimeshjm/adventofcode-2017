0e45beea56ce3c80623e84ba1106da63847650c0
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-05 22:50:11 +0000

    SAVEPOINT

---
+++ 2017/05/cs/InstructionSet/Jumps/JumpCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Jumps
+{
+    public class JumpCalculator
+    {
+        public int CalculateSteps(int[] steps)
+        {
+            var index = 0;
+            var counter = 0;
+            while (index < steps.Length)
+            {
+                index+= steps[index]++;
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
