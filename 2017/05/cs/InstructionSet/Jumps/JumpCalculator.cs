13a03dbff254772896018c89c00c805dbca26cef
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-05 23:02:08 +0000

    SAVEPOINT

--- 2017/05/cs/InstructionSet/Jumps/JumpCalculator.cs
+++ 2017/05/cs/InstructionSet/Jumps/JumpCalculator.cs
@@ -5,13 +5,13 @@ namespace Jumps
 {
     public class JumpCalculator
     {
-        public int CalculateSteps(int[] steps)
+        public int CalculateSteps(int[] steps, int part = 1)
         {
             var index = 0;
             var counter = 0;
             while (index < steps.Length)
             {
-                index+= steps[index]++;
+                index+= part == 2 && steps[index] >= 3 ? steps[index]-- : steps[index]++;
                 counter++;
             }
 
