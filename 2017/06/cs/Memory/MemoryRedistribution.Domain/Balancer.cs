35913985baeddac388122f72b082cab830720e8e
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-06 22:27:54 +0000

    SAVEPOINT

---
+++ 2017/06/cs/Memory/MemoryRedistribution.Domain/Balancer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MemoryRedistribution.Domain
+{
+    public class Balancer
+    {
+        public int Balance(int[] bankCount, int part)
+        {
+            var state = new List<int[]> ();
+
+            while (!state.Any(s => s.SequenceEqual(bankCount)))
+            {
+                state.Add(bankCount.ToArray());
+
+                var item = bankCount.Max();
+                var index = bankCount.ToList().IndexOf(item);
+
+                bankCount[index++ % bankCount.Length] = 0;
+
+                for (var i=0; i<item;i++)
+                {
+                    bankCount[index++ % bankCount.Length]++;
+                }
+            }
+
+            return state.Count;
+        }
+    }
+}
---
+++ 2017/06/cs/Memory/MemoryRedistribution.Tests/BalancerTests.cs
@@ -0,0 +1,20 @@
+﻿using MemoryRedistribution.Domain;
+using NUnit.Framework;
+
+namespace MemoryRedistribution.Tests
+{
+    [TestFixture]
+    public class BalancerTests
+    {
+        [TestCase(new[] { 0, 2, 7, 0 }, 5, 1)]
+        [TestCase(new[] { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 5042, 1)]
+        public void Test1(int[] bankCount, int expected, int part)
+        {
+            var sut = new Balancer();
+
+            var actual = sut.Balance(bankCount, part);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
