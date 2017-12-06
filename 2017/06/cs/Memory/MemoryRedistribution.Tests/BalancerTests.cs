c3038bd0ec30f742975931070c706c843df15135
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-06 22:38:22 +0000

    SAVEPOINT

--- 2017/06/cs/Memory/MemoryRedistribution.Tests/BalancerTests.cs
+++ 2017/06/cs/Memory/MemoryRedistribution.Tests/BalancerTests.cs
@@ -9,7 +9,7 @@ namespace MemoryRedistribution.Tests
         [TestCase(new[] { 0, 2, 7, 0 }, 5, 1)]
         [TestCase(new[] { 0, 2, 7, 0 }, 4, 2)]
         [TestCase(new[] { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 5042, 1)]
-        [TestCase(new[] { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 5042, 2)]
+        [TestCase(new[] { 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 1086, 2)]
         public void Test1(int[] bankCount, int expected, int part)
         {
             var sut = new Balancer();
