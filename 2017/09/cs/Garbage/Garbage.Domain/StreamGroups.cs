4439f04b22735774fe4f7540feb2a815f2b11147
Nimesh Manmohanlal <nimeshjm@gmail.com>
2017-12-21 00:21:21 +0000

    SAVEPOINT

---
+++ 2017/09/cs/Garbage/Garbage.Domain/StreamGroups.cs
@@ -0,0 +1,52 @@
+﻿namespace Garbage.Domain
+{
+    public class StreamGroups
+    {
+        public int Collect(string input)
+        {
+            var currentCount = 0;
+            var score = 0;
+            var skipNext = false;
+            var skipMultiple = false;
+
+            foreach (var element in input)
+            {
+                if (skipNext)
+                {
+                    skipNext = false;
+                    continue;
+                }
+
+                if (element == '!')
+                {
+                    skipNext = true;
+                }
+
+                if (skipMultiple)
+                {
+                    if (element == '>')
+                    {
+                        skipMultiple = false;
+                    }
+
+                    continue;
+                }
+
+                if (element == '{')
+                {
+                    currentCount++;
+                }
+                else if (element == '}')
+                {
+                    score += currentCount--;
+                }
+                else if (element == '<')
+                {
+                    skipMultiple = true;
+                }
+            }
+
+            return score;
+        }
+    }
+}
