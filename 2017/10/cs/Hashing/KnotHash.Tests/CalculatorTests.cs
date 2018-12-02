7bd3e675be2a7577010e334b1d121c183b9a9c28
Nimesh Manmohanlal <nimesh.manmohanlal@gamesys.co.uk>
2018-12-02 16:46:58 +0000

    SAVEPOINT

---
+++ 2017/10/cs/Hashing/KnotHash.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace KnotHash.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase(new [] {3, 4, 1, 5}, 5, 12)]
+        public void Test1(int[] lengths, int sequenceLength, int expected)
+        {
+            var sut = new Calculator(sequenceLength);
+
+            var actual = sut.MultiplyFirstTwo(lengths);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ 2017/10/cs/Hashing/KnotHash/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KnotHash
+{
+    public class Calculator
+    {
+        private readonly int sequenceLength;
+
+        public Calculator(int sequenceLength)
+        {
+            this.sequenceLength = sequenceLength;
+        }
+
+        public int MultiplyFirstTwo(int[] lengths)
+        {
+            var sequence = new LinkedList<int>(Enumerable.Range(0, sequenceLength));
+            var position = 0;
+            var skip = 0;
+
+            foreach (var reverseLength in lengths)
+            {
+                sequence.rev
+            }
+
+            return 0;
+        }
+    }
+}
