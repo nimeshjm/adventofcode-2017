25c16d4b5e32e8a01ee7ed9b89e26193e9a87724
Nimesh Manmohanlal <nimesh.manmohanlal@gamesys.co.uk>
2018-12-02 17:26:50 +0000

    SAVEPOINT

--- 2018/01/main.go
+++ 2018/01/main.go
@@ -8,21 +8,22 @@ import (
 )
 
 func main() {
+	exercise1()
+}
+
+func exercise1() {
 	file, err := os.Open("input1.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
-	var frequency int = 0;
+	var frequency int = 0
 	for scanner.Scan() {
-		var row,_ = strconv.Atoi(scanner.Text());
+		var row, _ = strconv.Atoi(scanner.Text());
 		frequency += row;
 	}
-
-	log.Print("Frequency: ", frequency);
-
+	log.Print("Frequency: ", frequency)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
