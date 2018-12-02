c8b662d2330e268f6c26670fa19e6199e7b6a277
Nimesh Manmohanlal <nimesh.manmohanlal@gamesys.co.uk>
2018-12-02 17:07:50 +0000

    SAVEPOINT

---
+++ 2018/01/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var frequency int = 0;
+	for scanner.Scan() {
+		var row,_ = strconv.Atoi(scanner.Text());
+		frequency += row;
+	}
+
+	log.Print("Frequency: ", frequency);
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
