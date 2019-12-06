0a188d82135c17a7c3476d82038b43a0510a6e7a
Nimesh Manmohanlal <nimesh.manmohanlal@gamesys.co.uk>
2019-12-06 23:31:17 +0000

    SAVEPOINT

--- 2018/01/main.go
+++ 2018/01/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	exercise1()
+	exercise2()
 }
 
 func exercise1() {
@@ -18,13 +18,57 @@ func exercise1() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	var frequency int = 0
+	var frequency = 0
 	for scanner.Scan() {
-		var row, _ = strconv.Atoi(scanner.Text());
-		frequency += row;
+		var row, _ = strconv.Atoi(scanner.Text())
+		frequency += row
 	}
 	log.Print("Frequency: ", frequency)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func exercise2() {
+	file, err := os.Open("input1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	var frequency = 0
+	m := map[int]map[int]int{}
+	m[0] = make(map[int]int)
+	m[1] = make(map[int]int)
+	var frequencyFound = false
+
+	for !frequencyFound {
+		file.Seek(0,0)
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			var row, _= strconv.Atoi(scanner.Text())
+			frequency += row
+
+			found := m[1][frequency]
+			if found != 0 {
+				log.Println("Twice ", found)
+				frequencyFound = true
+				break
+			}
+
+			found = m[0][frequency]
+			if found != 0 {
+				delete(m[0], frequency)
+				m[1][frequency] = 1
+			} else {
+				m[0][frequency] = 1
+				found = m[0][frequency]
+			}
+			log.Println("m0 ", len(m[0]))
+			log.Println("m1 ", len(m[1]))
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
