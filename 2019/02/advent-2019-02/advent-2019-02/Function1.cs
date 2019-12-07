17f12d65e7b5b21ea4ddad42af80f9c223c01dd2
Nimesh Manmohanlal <nimesh.manmohanlal@gamesys.co.uk>
2019-12-07 00:39:31 +0000

    brute force

--- 2019/02/advent-2019-02/advent-2019-02/Function1.cs
+++ 2019/02/advent-2019-02/advent-2019-02/Function1.cs
@@ -5,7 +5,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using System.Linq;
-using System;
 using Microsoft.Extensions.Logging;
 
 namespace advent_2019_02
@@ -13,24 +12,64 @@ namespace advent_2019_02
     public static class Function1
     {
         [FunctionName("part1")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> Run1(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             long[] values = requestBody.Split(',').Select(long.Parse).ToArray();
 
-            long i = 0;
-            while(i < values.Length && values[i] != 99) {
+            Part1(values);
 
-                values[values[i + 3]] = values[i] == 1 
-                    ? values[values[i + 1]] + values[values[i + 2]] 
+            return new OkObjectResult($"{values[0]}");
+        }
+
+        [FunctionName("part2")]
+        public static async Task<IActionResult> Run2(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            long result = 0;
+            long output = 0;
+            int i = 0;
+            int k = 0;
+            do
+            {
+                long[] values = requestBody.Split(',').Select(long.Parse).ToArray();
+                values[1] = i;
+                values[2] = k;
+                Part1(values);
+                result = values[0];
+                output = 100 * values[1] + values[2];
+                i++;
+                if (i==100)
+                {
+                    i = 0;
+                    k++;
+                }
+                log.LogInformation($"{i}:{k} - {result} - {output}");
+            } while (result != 19690720);
+
+            return new OkObjectResult($"{output}");
+        }
+
+
+
+
+        private static void Part1(long[] values)
+        {
+            long i = 0;
+            while (i < values.Length && values[i] != 99)
+            {
+
+                values[values[i + 3]] = values[i] == 1
+                    ? values[values[i + 1]] + values[values[i + 2]]
                     : values[values[i + 1]] * values[values[i + 2]];
 
                 i += 4;
             }
-
-
-            return (ActionResult)new OkObjectResult($"{values[0]}");
         }
+
+
     }
 }
